3f05a0f52e715e789014abef2a66413a38472dd1
ebjordal <ebjordal@gmail.com>
2017-12-12 22:41:21 +0100

    Merge branch 'master' of https://github.com/ebjordal/sbmachine
    # Conflicts:
    #	sbmachine.py

--- sbmachine.py
+++ sbmachine.py
@@ -21,7 +21,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def login(self):
         dmw.hide()
         dmf.show()
-        UserLoginM.user_email.clear()
+        dmf.user_email.clear()
 
 
 class UserLoginM(QDialog, Ui_userlogin_manual):
@@ -52,6 +52,7 @@ class UserLoginM(QDialog, Ui_userlogin_manual):
         self.pushButton_r.clicked.connect(self.LoginPushR)
         self.pushButton_s.clicked.connect(self.LoginPushS)
         self.pushButton_t.clicked.connect(self.LoginPushT)
+        self.pushButton_u.clicked.connect(self.LoginPushU)
         self.pushButton_v.clicked.connect(self.LoginPushV)
         self.pushButton_w.clicked.connect(self.LoginPushW)
         self.pushButton_x.clicked.connect(self.LoginPushX)
@@ -60,21 +61,27 @@ class UserLoginM(QDialog, Ui_userlogin_manual):
         self.pushButton_dot.clicked.connect(self.LoginPushDot)
         self.pushButton_dash.clicked.connect(self.LoginPushDash)
         self.pushButton_alfa.clicked.connect(self.LoginPushAlpha)
-
-
-
-
-
-
+        self.pushButton_one.clicked.connect(self.LoginPushOne)
+        self.pushButton_two.clicked.connect(self.LoginPushTwo)
+        self.pushButton_three.clicked.connect(self.LoginPushThree)
+        self.pushButton_four.clicked.connect(self.LoginPushFour)
+        self.pushButton_five.clicked.connect(self.LoginPushFive)
+        self.pushButton_six.clicked.connect(self.LoginPushSix)
+        self.pushButton_seven.clicked.connect(self.LoginPushSeven)
+        self.pushButton_eight.clicked.connect(self.LoginPushEight)
+        self.pushButton_nine.clicked.connect(self.LoginPushNine)
+        self.pushButton_zero.clicked.connect(self.LoginPushZero)
 
     def submit(self):
+        dmp.pincode_entry.clear()
         dmf.hide()
         dmp.show()
 
     def cancel(self):
-        self.user_email.clear()
+        dmf.user_email.clear()
         dmf.hide()
         dmw.show()
+
 
     def LoginPushA(self):
         self.user_email.insert("a")
@@ -134,6 +141,26 @@ class UserLoginM(QDialog, Ui_userlogin_manual):
         self.user_email.insert("-")
     def LoginPushAlpha(self):
         self.user_email.insert("@")
+    def LoginPushOne(self):
+        self.user_email.insert("1")
+    def LoginPushTwo(self):
+        self.user_email.insert("2")
+    def LoginPushThree(self):
+        self.user_email.insert("3")
+    def LoginPushFour(self):
+        self.user_email.insert("4")
+    def LoginPushFive(self):
+        self.user_email.insert("5")
+    def LoginPushSix(self):
+        self.user_email.insert("6")
+    def LoginPushSeven(self):
+        self.user_email.insert("7")
+    def LoginPushEight(self):
+        self.user_email.insert("8")
+    def LoginPushNine(self):
+        self.user_email.insert("9")
+    def LoginPushZero(self):
+        self.user_email.insert("0")
     def LoginPushDel(self):
         self.user_email.backspace()
 
@@ -144,8 +171,43 @@ class PinCode(QDialog,Ui_pincode):
         self.setupUi(self)
         self.pushbutton_ok.clicked.connect(self.submit)
         self.pushButton_cancel.clicked.connect(self.cancel)
+        self.pushButton_one.clicked.connect(self.PinButtonOne)
+        self.pushButton_two.clicked.connect(self.PinButtonTwo)
+        self.pushButton_three.clicked.connect(self.PinButtonThree)
+        self.pushButton_four.clicked.connect(self.PinButtonFour)
+        self.pushButton_five.clicked.connect(self.PinButtonFive)
+        self.pushButton_six.clicked.connect(self.PinButtonSix)
+        self.pushButton_seven.clicked.connect(self.PinButtonSeven)
+        self.pushButton_eight.clicked.connect(self.PinButtonEight)
+        self.pushButton_nine.clicked.connect(self.PinButtonNine)
+        self.pushButton_zero.clicked.connect(self.PinButtonZero)
+
+    def PinButtonOne(self):
+        self.pincode_entry.insert("1")
+    def PinButtonTwo(self):
+        self.pincode_entry.insert("2")
+    def PinButtonThree(self):
+        self.pincode_entry.insert("3")
+    def PinButtonFour(self):
+        self.pincode_entry.insert("4")
+    def PinButtonFive(self):
+        self.pincode_entry.insert("5")
+    def PinButtonSix(self):
+        self.pincode_entry.insert("6")
+    def PinButtonSeven(self):
+        self.pincode_entry.insert("7")
+    def PinButtonEight(self):
+        self.pincode_entry.insert("8")
+    def PinButtonNine(self):
+        self.pincode_entry.insert("9")
+    def PinButtonZero(self):
+        self.pincode_entry.insert("0")
+
+
 
     def submit(self):
+        print(self.pincode_entry.text())
+        print("Hello")
         dmp.hide()
         dmw.show()
 
